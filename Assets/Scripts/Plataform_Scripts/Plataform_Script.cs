efd6a5cb90752b6d6642cb40be2af9d8473c6e68
Kim_10 <quimbaltrons@gmail.com>
2026-03-16 11:54:17 +0100

    Plataform update

--- Assets/Scripts/Plataform_Scripts/Plataform_Script.cs
+++ Assets/Scripts/Plataform_Scripts/Plataform_Script.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Plataform_Script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform posA, posB;
+    public float speed;
+    Vector3 targetPos;
+
+    private void Start()
     {
-        
+        targetPos = posB.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Vector2.Distance(transform.position, posA.position) < 0.05f)
+        {
+            targetPos = posB.position;
+        }
+
+        if (Vector2.Distance(transform.position, posB.position) < 0.05f)
+        {
+            targetPos = posA.position;
+        }
         
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
     }
 }
